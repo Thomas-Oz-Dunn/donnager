8c22730d54ce158e29453edfb69ab486cf22a0ae
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2022-12-03 17:43:42 -0600

    all: calculate fuel costs, mass flow

--- src/constants.rs
+++ src/constants.rs
@@ -5,12 +5,15 @@ TODO-TD: query JPL SSDs
 */
 
 // Math
-pub const PI: f64 = 3.1415926535;
-pub const DEG_TO_RAD: f64 = PI / 180.0;
+// pub const PI: f64 = 3.1415926535;
+// pub const DEG_TO_RAD: f64 = PI / 180.0;
 
 // Space
-pub const SUN_MASS: f64 = 1.9891e30; // kg
+// pub const SUN_MASS: f64 = 1.9891e30; // kg
+pub const EARTH_MASS: f64 = 5.972e24; // kg
+pub const EARTH_RADIUS_EQUATOR: f64 = 6.378137e6; // m
 
 // Physics
 pub const GRAV_CONST: f64 = 6.6743015e-11; // m^3 * kg^-1 * s^-2
-pub const SPEED_OF_LIGHT: f64 = 2.99792458e8; // m * s^-1
+pub const GAS_CONST: f64 = 8.317e3; // N * m * kg^-1 * mol^-1 * K^-1
+// pub const SPEED_OF_LIGHT: f64 = 2.99792458e8; // m * s^-1
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,98 @@
+/*
+Astrodynamics in Rust
+
+*/
 mod constants;
 
 fn main() {
-    let mass_0: f64 = 5.098e35; // kg
-    let mass_1: f64 = 2.4; // kg
+    let mass_0: f64 = constants::EARTH_MASS;
+    let radius_0: f64 = constants::EARTH_RADIUS_EQUATOR; 
 
-    let radius_0: f64 = 344444.0; //m
-    let radius_f: f64 = 34444400.0; //m
+    let mass_1: f64 = 1.0; // kg
+    let altitude: f64 = 3.0e6; // 300km, LEO
+    let radius_f: f64 = radius_0 + altitude; // m
+    let engine_isp: f64 = 300.0; // s
 
-    let engine_isp: f64 = 543.123; // s
-
-    let delta_v = delta_v(mass_0, radius_0, radius_f);
-    let g_0: f64 = constants::GRAV_CONST * mass_0 / radius_0.powf(2.0);
-    let mass_fuel = mass_fuel(delta_v, mass_1, engine_isp, g_0);    
-    println!("{} kg of fuel", mass_fuel)
+    let delta_v: f64 = orbital_velocity(mass_0, radius_f);
+    let grav_acc: f64 = grav_acc(mass_0, radius_0);
+    let mass_fuel: f64 = mass_fuel(delta_v, mass_1, engine_isp, grav_acc);    
+    println!("{} kg of fuel", mass_fuel);
 }
 
-fn orbital_velocity(
+/*
+TODO-TD: 
+N-stage trade study
+TLE ingest
+Hohman Transfers
+Trans Lunar Injections
+Multithreading, Cloud Compute?
+RK45 propogator
+J2 perturbation
+Launch cost calculator
+Solar System Mineralogical data base query
+Interplanetary Mission Plan (optimal launch windows, porkchop plot)
+$ / kg (mineral X)
+*/
+
+
+pub fn grav_acc(
     mass_0: f64,
     radius: f64
 ) -> f64{
-    // KE = PE
-    // 1/2 m_1 * v^2 = m_1 * g * r
-    // g = F_g / m_1
-    // F_g = G * m_1 * m_0 / r^2
-    // g = G * m_0 / r^2
-    // v = sqrt(2 * g * r)
-    // v - sqrt(2 * G * m_0 / r)
-    let energy = 2.0 * GRAV_CONST * mass_0 / radius;
-    let velocity = energy.sqrt();
+    let grav_acc: f64 = constants::GRAV_CONST * mass_0 / radius.powi(2);
+    grav_acc
+}
+
+pub fn orbital_velocity(
+    mass_0: f64,
+    radius: f64
+) -> f64{
+    let energy: f64 = 2.0 * constants::GRAV_CONST * mass_0 / radius;
+    let velocity: f64 = energy.sqrt();
     velocity
 }
 
-// 1 dimensional, what about hohman transfer and similar?
-fn delta_v(
-    mass_0: f64,
-    radius_0: f64,
-    radius_f: f64
-) -> f64{
-    /*
-    mass_0 : float64
-        Mass of central body
-
-    radius_0 : float64
-        Initial radius
-    
-    radius_f : float64
-        Final velocity
-    */
-    
-    let v_0: f64 = orbital_velocity(mass_0, radius_0);
-    let v_f: f64 = orbital_velocity(mass_0, radius_f);
-    let delta_v: f64 = v_f - v_0;
-    delta_v
-}
-
-fn mass_fuel(
+pub fn mass_fuel(
     delta_v: f64,
     mass_1: f64,
     engine_isp: f64,
-    g_0: f64
+    grav_acc: f64
 ) -> f64{
-    let power = delta_v / (engine_isp * g_0);
-    let mass_f = mass_1 * power.exp();
-    mass_f
+    let v_exhaust: f64 = engine_isp * grav_acc;
+    let power: f64 = delta_v / v_exhaust;
+    let mass_fuel: f64 = mass_1 * (power.exp() - 1.0);
+    mass_fuel
+}
+
+pub fn apogee_height(
+    mass_ratio: f64,
+    engine_isp: f64,
+    grav_acc: f64,
+    acc_ratio: f64
+) -> f64{
+    let z: f64 = 1.0 - mass_ratio;
+    let x: f64 = (1.0 / (z)).ln() - 1.0 / acc_ratio;
+    let v_bo: f64 = grav_acc * engine_isp * (x);
+    let coast_height: f64 = v_bo.powi(2) / (2.0 * grav_acc);
+    
+    let y: f64 = z * (z).ln() + mass_ratio - mass_ratio.powi(2) / 2.0;
+    let burnout_height: f64 = grav_acc * engine_isp.powi(2) * y;
+    let apogee_height: f64 = burnout_height + coast_height;
+    apogee_height
+}
+
+
+pub fn calc_mass_flow(
+    throat_area: f64,
+    pressure_chamber: f64,
+    temp_chamber: f64,
+    gamma: f64,
+    mol_weight: f64
+) -> f64 {
+    let a: f64 = (2.0/(gamma + 1.0)).powf((gamma + 1.0)/(2.0*(gamma - 1.0))); 
+    let b: f64 = ((constants::GAS_CONST * temp_chamber) / gamma * mol_weight).sqrt();
+    let c_star: f64 = b * a;
+
+    let mass_flow: f64 = pressure_chamber * throat_area / c_star; 
+    mass_flow
 }
