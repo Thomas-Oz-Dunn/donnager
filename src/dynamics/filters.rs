7f7073cb20abc74eb1de54493effe99f2152f38c
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2022-12-24 14:39:02 -0600

    filters.rs: variable naming, system noise covariance calculation

--- src/dynamics/filters.rs
+++ src/dynamics/filters.rs
@@ -5,57 +5,60 @@ use nalgebra::*;
 #[derive(Clone, Debug, PartialEq)]
 
 pub struct KalmanFilter{
-    pub sys_trans: DMatrix<f64>,
-    pub observ_trans: DMatrix<f64>,
-    pub cntrl_trans: DMatrix<f64>,
-    pub prcx_nosie_cov: DMatrix<f64>,
-    pub obserb_noise_cov: DMatrix<f64>,
-    pub system: DMatrix<f64>,
-    pub post_noise_cov: DMatrix<f64>
+    pub syst_tm: DMatrix<f64>,
+    pub obsv_tm: DMatrix<f64>,
+    pub ctrl_tm: DMatrix<f64>,
+    pub prcx_noise_cov: DMatrix<f64>,
+    pub obsv_noise_cov: DMatrix<f64>,
+    pub syst_noise_cov: DMatrix<f64>,
+    pub system: DMatrix<f64>
 }
 
 impl KalmanFilter{
     
     pub fn predict(&mut self, control: DVector<f64>) -> DMatrix<f64> {
-        let system: DMatrix<f64> = &self.sys_trans * self.system.transpose() + 
-            &self.cntrl_trans * control.transpose();
+
+        // System (x) := stm * x + ctm * controls
+        let system: DMatrix<f64> = 
+            &self.syst_tm * self.system.transpose() + 
+            &self.ctrl_tm * control.transpose();
         self.system = system.clone();
-        self.post_noise_cov = 
-            (&self.sys_trans * self.post_noise_cov.transpose()) * &self.sys_trans 
-            + &self.obserb_noise_cov;
+
+        // Posteriori noise covariance (P) := inner(stm, P) * stm + obs_noise_cov
+        self.syst_noise_cov = 
+            (&self.syst_tm * self.syst_noise_cov.transpose()) * &self.syst_tm 
+            + &self.obsv_noise_cov;
         return system
     }
 
-    pub fn update(&self, measurement: DVector<f64>) {
-        let y: DVector<f64> = 
-            measurement - &self.observ_trans * self.system.transpose();
-        let S: DMatrix<f64> = 
-            &self.obserb_noise_cov + self.observ_trans * 
-            (self.prcx_nosie_cov * self.observ_trans).transpose();
+    pub fn update(&mut self, measurement: DVector<f64>) {
 
-        let K: DMatrix<f64> = 
-            (self.prcx_nosie_cov * self.observ_trans) * S.try_inverse().unwrap().transpose();
-        self.system = self.system + K * y.transpose();
+        // y = observed - inner(otm, system)
+        let innovation: DVector<f64> = 
+            measurement - &self.obsv_tm * self.system.transpose();
+        
+        // system noise = obsv_noise_cov + otm * prcx noise * otm 
+        let innovation_cov: DMatrix<f64> = 
+            &self.obsv_noise_cov + &self.obsv_tm * 
+            (&self.prcx_noise_cov * &self.obsv_tm.transpose());
+
+        let kalman_gain: DMatrix<f64> = 
+            (&self.prcx_noise_cov * &self.obsv_tm.transpose()) * 
+            innovation_cov.try_inverse().unwrap();
+
+        // x :=  x + inner(K, y)
+        let system: DMatrix<f64> = &self.system + &kalman_gain * innovation.transpose();
+        self.system = system.clone();
 
         let dims: (usize, usize) = self.system.shape();
-        let iden: DMatrix<f64> = 
+        let identity: DMatrix<f64> = 
             DMatrix::<f64>::identity(dims.0, dims.1);
+        let z: DMatrix<f64> = 
+            &identity - &kalman_gain * self.obsv_tm.transpose();
 
-        self.post_noise_cov = 
-            np.dot(
-                np.dot(
-                    iden - np.dot(
-                        K, 
-                        self.observ_trans), 
-                    self.P), 
-        	    (iden - np.dot(
-                    K, 
-                    self.observ_trans)).T) 
-            + np.dot(
-                np.dot(
-                    K, 
-                    self.R), 
-                K.T)
+        self.syst_noise_cov = 
+            (&z * self.syst_noise_cov.transpose()) * &z + 
+            (&kalman_gain * self.obsv_noise_cov.transpose()) * &kalman_gain;
     }
 
 }
