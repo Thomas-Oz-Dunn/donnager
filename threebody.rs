dbb442667f3d9077a1c2b8e8f5a66202b37d9d9c
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-01-13 14:54:18 -0800

    threebody.rs: use mutable variables to save memory

--- threebody.rs
+++ threebody.rs
@@ -32,26 +32,33 @@ fn calc_acceleration(
     particles: Vec<Particle>,
     field_strength: f64
 ) -> Vec<Vec3<f64>> {
-
-    let acc: Vec<Vec3<f64>>;
+    let mut distance: Vec3<f64>;
+    let mut acc: Vec<Vec3<f64>>;
+    let mut radius: f64;
+    let mut force: f64;
 
     // O(n^2)
     // Dude there is a lot of redundant code, binary sort this shit
     for (idx1, particle1) in particles.enumerate(){
         for (idx2, particle2) in particles[idx..].enumerate(){
-            let distance: Vec3<f64> = particle1.pos - particle2.pos;
-            let radius: f64 = distance.norm();
-            let force: f64 =  field_strength * (particle1.mass + particle2.mass) / radius.powi(2);
-            let dir: Vec3<f64> = distance / radius;
-            calc_accelerations[idx1] += -force / particle1.mass * dir;
-            calc_accelerations[idx2] += force / particle2.mass * dir;
-
+            distance = particle1.pos - particle2.pos;
+            radius = distance.norm();
+            force =  field_strength * (particle1.mass + particle2.mass) / radius.powi(2);
+            calc_accelerations[idx1] += -force / particle1.mass * distance / radius;
+            calc_accelerations[idx2] += force / particle2.mass * distance / radius;
         }
     }
     return acc
 }
 
 
+
+/// Simulation builds to n body
+/// 
+/// 1 body - nada
+/// 2 body - single for loop
+/// 3 body - vectorized
+/// n body - barnes hut tree
 
 fn main() {
 
@@ -82,10 +89,12 @@ fn main() {
 
     // Control duration and precision
     for dt in (0..t_end).iter(){
+
         distance = particle1.pos - particle2.pos;
         radius = distance.norm();
         force = field_strength * (particle1.mass + particle2.mass) / radius.powi(2);
         dir = distance / radius;
+
         acc_1 = -force / particle1.mass * dir;
         acc_2 = force / particle2.mass * dir;
 
