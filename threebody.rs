f40b12b30632c0dec3d42d4221454017844f9046
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-01-13 14:47:31 -0800

    threebody.rs: begin vectorization

--- threebody.rs
+++ threebody.rs
@@ -27,19 +27,28 @@ impl Tree {
 }
 
 
-/// Calculate 2-body acceleration, brute force
+/// Calculate acceleration, brute force
 fn calc_acceleration(
-    particle1: Particle,
-    particle2: Particle,
+    particles: Vec<Particle>,
     field_strength: f64
-) -> (Vec3<f64>, Vec3<f64>) {
-    let distance: Vec3<f64> = particle1.pos - particle2.pos;
-    let radius: f64 = distance.norm();
-    let force: f64 =  field_strength * (particle1.mass + particle2.mass) / radius.powi(2);
-    let dir: Vec3<f64> = distance / radius;
-    let acc_1: Vec3<f64> = -force / particle1.mass * dir;
-    let acc_2: Vec3<f64> = force / particle2.mass * dir;
-    return (acc_1, acc_2)
+) -> Vec<Vec3<f64>> {
+
+    let acc: Vec<Vec3<f64>>;
+
+    // O(n^2)
+    // Dude there is a lot of redundant code, binary sort this shit
+    for (idx1, particle1) in particles.enumerate(){
+        for (idx2, particle2) in particles[idx..].enumerate(){
+            let distance: Vec3<f64> = particle1.pos - particle2.pos;
+            let radius: f64 = distance.norm();
+            let force: f64 =  field_strength * (particle1.mass + particle2.mass) / radius.powi(2);
+            let dir: Vec3<f64> = distance / radius;
+            calc_accelerations[idx1] += -force / particle1.mass * dir;
+            calc_accelerations[idx2] += force / particle2.mass * dir;
+
+        }
+    }
+    return acc
 }
 
 
@@ -50,7 +59,7 @@ fn main() {
     let mut particle1: Particle = Particle {
         pos = Vec3::new(0.,0.,0.),
         vel = Vec3::new(0.,0.,1.),
-        mass = 5.0
+        mass = 3.0
     };
 
     // 2 body
@@ -64,10 +73,21 @@ fn main() {
     // Run
     let t_end: u_size = 60;
     let field_strength: f64 = 4.0;
+    let mut distance: Vec3<f64>;
+    let mut radius: f64;
+    let mut force: f64;
+    let mut dir: Vec3<f64>;
+    let mut acc_1: Vec3<f64>;    
+    let mut acc_2: Vec3<f64>;
 
     // Control duration and precision
     for dt in (0..t_end).iter(){
-        (acc1, acc2) = calc_acceleration(particle1, particle2, field_strength);
+        distance = particle1.pos - particle2.pos;
+        radius = distance.norm();
+        force = field_strength * (particle1.mass + particle2.mass) / radius.powi(2);
+        dir = distance / radius;
+        acc_1 = -force / particle1.mass * dir;
+        acc_2 = force / particle2.mass * dir;
 
         particle1.pos += particle1.vel * dt;
         particle2.pos += particle2.vel * dt;
@@ -77,6 +97,26 @@ fn main() {
 
     }
 
-    // 3 body
+    // 3 body - initial vectorization
+    let mut particle3: Particle = Particle {
+        pos = Vec3::new(0.,0.,3.),
+        vel = Vec3::new(2.,0.,0.),
+        mass = 7.0
+    };
+
+    let mut particles: vec![particle1, particle2, particle3];
+
+    for dt in (0..t_end).iter(){
+        let accel: Vec<Vec3<f64>> = calc_acceleration(particles, field_strength);
+        particles.zip(accel.iter()).for_each(|(particle, accel)| {
+            particle.pos += particle.vel * dt;
+            particle.vel += accel * dt;
+        });
+
+    }
+
+
+
+    // N body- watch your Kolmogrov Complexity
 
 }
