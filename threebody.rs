f0de08323a5676de114d7103b3dab930e7860b58
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-01-13 14:25:59 -0800

    threebody.rs: calc_acceleration()

--- threebody.rs
+++ threebody.rs
@@ -9,17 +9,17 @@ pub struct Particle {
     pub mass: f64,
 }
 
-
-// NOTE: Make agnostic to 1, 2, 3, N d space?
-
-pub struct Node {}
+pub struct Node {
+    pub mass: f64
+}
 
 impl Node {
-    
+
 }
 
 pub struct Tree {
-    pub nodes: Vec<Node>
+    pub nodes: Vec<Node>,
+    pub center_of_mass: Vec3<f64>
 }
 
 impl Tree {
@@ -27,8 +27,20 @@ impl Tree {
 }
 
 
-
-
+/// Calculate 2-body acceleration, brute force
+fn calc_acceleration(
+    particle1: Particle,
+    particle2: Particle,
+    field_strength: f64
+) -> (Vec3<f64>, Vec3<f64>) {
+    let distance: Vec3<f64> = particle1.pos - particle2.pos;
+    let radius: f64 = distance.norm();
+    let force: f64 =  field_strength * (particle1.mass + particle2.mass) / radius.powi(2);
+    let dir: Vec3<f64> = distance / radius;
+    let acc_1: Vec3<f64> = -force / particle1.mass * dir;
+    let acc_2: Vec3<f64> = force / particle2.mass * dir;
+    return (acc_1, acc_2)
+}
 
 
 
@@ -50,7 +62,20 @@ fn main() {
 
     
     // Run
+    let t_end: u_size = 60;
+    let field_strength: f64 = 4.0;
 
+    // Control duration and precision
+    for dt in (0..t_end).iter(){
+        (acc1, acc2) = calc_acceleration(particle1, particle2, field_strength);
+
+        particle1.pos += particle1.vel * dt;
+        particle2.pos += particle2.vel * dt;
+
+        particle1.vel += acc1 * dt;
+        particle2.vel += acc2 * dt;
+
+    }
 
     // 3 body
 
