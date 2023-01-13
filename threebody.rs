9c93b612202310c80fd8d8169169355d11845740
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-01-12 18:56:56 -0800

    threebody.rs: initial commit

---
+++ threebody.rs
@@ -0,0 +1,16 @@
+/*
+Three body simulation demonstration
+*/
+
+fn main() {
+    // 1 body
+    let body1: f64;
+
+    // 2 body
+    let body2: f64;
+    let time_duration: usize = 60;
+    
+
+    // 3 body
+
+}
