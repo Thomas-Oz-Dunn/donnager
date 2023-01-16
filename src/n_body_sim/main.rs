8ce435aa30e99d69cb1e50be304861aa39501aaa
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-01-15 19:52:27 -0800

    main.rs: successful plotting!

--- src/n_body_sim/main.rs
+++ src/n_body_sim/main.rs
@@ -7,7 +7,6 @@ use std::fs;
 use std::ops::Range;
 
 use plotters::prelude::*;
-use plotters::coord::types::*;
 use nalgebra as na;
 use na::Vector3;
 
@@ -46,17 +45,18 @@ fn calc_acceleration(
     let mut distance: Vector3<f64>;
     let mut radius: f64;
     let mut force: f64;
-    let mut acc_mut: Vec<Vector3<f64>> = Vec::with_capacity(particles.len());
+    let zero_vec: Vector3<f64> = Vector3::new(0.0, 0.0, 0.0);
+    let mut acc_mut: Vec<Vector3<f64>> = vec![zero_vec; particles.len()];
 
     // O(n^2)
     // Dude there is a lot of redundant code, binary sort this shit
     for (idx1, particle1) in particles.iter().enumerate(){
-        for (idx2, particle2) in particles[idx1..].iter().enumerate(){
+        for (idx2, particle2) in particles[idx1..particles.len()].iter().enumerate(){
             distance = particle1.pos - particle2.pos;
             radius = distance.norm();
             force =  field_strength * (particle1.mass + particle2.mass) / radius.powi(2);
 
-            acc_mut[idx1] += -force / particle1.mass * distance / radius;
+            acc_mut[idx1] -= force / particle1.mass * distance / radius;
             acc_mut[idx2] += force / particle2.mass * distance / radius;
         }
     }
@@ -77,22 +77,23 @@ fn main() {
 
     // 1 body
     let mut particle1: Particle = Particle {
-        pos: Vector3::new(0.,0.,0.),
-        vel: Vector3::new(0.,0.,1.),
-        mass: 3.0
+        pos: Vector3::new(0.9,2.8,0.),
+        vel: Vector3::new(0.2,-0.1, 0.),
+        mass: 30.0
     };
 
     // 2 body
     let mut particle2: Particle = Particle {
-        pos: Vector3::new(1.,0.,0.),
-        vel: Vector3::new(0.,-2.,0.),
-        mass: 5.0
+        pos: Vector3::new(0.7,0.5,0.),
+        vel: Vector3::new(-0.1,0.07,0.),
+        mass: 50.0
     };
 
     match fs::create_dir("./images") {
         Err(why) => println!("! {:?}", why.kind()),
         Ok(_) => {},
     }
+
     let root_drawing_area = BitMapBackend::gif(
         "./images/animated.gif", 
         (500, 500), 
@@ -103,14 +104,15 @@ fn main() {
     let mut ctx = ChartBuilder::on(&root_drawing_area)
         .set_label_area_size(LabelAreaPosition::Left, 30)
         .set_label_area_size(LabelAreaPosition::Bottom, 30)
-        .build_cartesian_2d::<Range<i32>, Range<i32>>(0..100, 0..100)
+        .build_cartesian_2d::<Range<f64>, Range<f64>>(-10.0..10.0, -10.0..10.0)
         .unwrap();
 
     ctx.configure_mesh().draw().unwrap();
 
+
     // Run
     let t_end: i32 = 60;
-    let field_strength: f64 = 4.0;
+    let field_strength: f64 = 500.0;
     let mut distance: Vector3<f64>;
     let mut radius: f64;
     let mut force: f64;
@@ -132,16 +134,16 @@ fn main() {
         particle1.pos += particle1.vel;
         particle2.pos += particle2.vel;
 
-        particle1.vel += acc_2;
+        particle1.vel += acc_1;
         particle2.vel += acc_2;
 
     }
 
     // 3 body - initial vectorization
     let mut particle3: Particle = Particle {
-        pos: Vector3::new(0.,0.,3.),
+        pos: Vector3::new(0.,0.,0.),
         vel: Vector3::new(2.,0.,0.),
-        mass: 7.0
+        mass: 0.0
     };
 
     let mut particles: Vec<Particle> = Vec::new();
@@ -152,18 +154,25 @@ fn main() {
         let accel: Vec<Vector3<f64>> = calc_acceleration(particles.clone(), field_strength);
         (0..).zip(
             accel.iter().zip(
-                particles.iter()))
-                .map(|(i_point, (acc, particle))| {
-            
-                ctx.draw_series(LineSeries::new(
-                    [particle.pos[0], particle.pos[1]].iter(), &Palette99::pick(i_point)
-                )).unwrap();
-            
+                particles.iter_mut()))
+                    .for_each(|(i_point, (acc, particle))| {
+                        ctx.draw_series(
+                            // PLot idea, circle at point!
+                            LineSeries::new(
+                                [(particle.pos[0], particle.pos[1]), (particle.pos[0] + particle.vel[0], particle.pos[1] + particle.vel[1])], 
+                                Palette99::pick(i_point))
+                            ).unwrap().label(format!("Particle {}", i_point));
+
                 particle.pos += particle.vel;
                 particle.vel += acc;
             });
 
     }
+    ctx.configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()
+        .unwrap();
 
 
 
