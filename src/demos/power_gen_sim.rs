ec0a140761914dce1cd99e2535b3f738e50e2da5
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-02-14 21:51:00 -0500

    power_gen_sim.rs: work backwards from radiation force calc

--- src/demos/power_gen_sim.rs
+++ src/demos/power_gen_sim.rs
@@ -1,22 +1,43 @@
 /*
 Solar power generation simulation for sun-synchronous orbit
 */
+use nalgebra as na;
+use na::Vector3;
 
 use donnager::constants as cst;
 use donnager::cosmos as cosm;
-use donnager::dynamics as dynam;
 
-let sample_tle: &str = 
-    "ICEYE X1
-    1 43114U 18004D   23045.46121586  .00040042 00000-0  11420-2 0  9993
-    2 43114  97.3365 121.5406 0007153 204.3285 155.7622 15.35923585283273";
+fn main(){
+    let sample_tle: &str = 
+        "ICEYE X1
+        1 43114U 18004D   23045.46121586  .00040042 00000-0  11420-2 0  9993
+        2 43114  97.3365 121.5406 0007153 204.3285 155.7622 15.35923585283273";
     
-let sso_orbit: Orbit = cosm::gravity::Orbit.from_tle(sample_tle);
+    let sso_orbit: cosm::gravity::Orbit = cosm::gravity::Orbit::from_tle(sample_tle.to_string());
+    
+    // Calculate occlusion events
+    // ECI coordinates?
+    // Where is sun in ECI @ ref datetime?
+    
+    let veh_pos_ecef = Vector3::<f64>.ones();
+    let sun_pos_ecef = Vector3::<f64>.zeros();
 
-// Calculate occlusion events
+    // Space params
+    let src_obj_vec: Vector3<f64> = veh_pos_ecef - sun_pos_ecef;
+    let em_flux: f64 = cst::SUN_MEAN_SOLAR_FLUX;
 
-let pos = sso_orbit.;
-// Calculate flux over time
+    // Vehicle paramsss
+    let obj_reflecitivity: f64 = 1.6;
+    let normal_area: f64 = 1;
 
-let solar_pressure = cosm::electromagnetism::calc_em_radiative_force()
-// Replicate / Falsify the results Dae Lee's paper, cite?
+    let solar_pressure = cosm::electromagnetism::calc_em_radiative_force(
+        em_flux,
+        src_obj_vec,
+        obj_reflecitivity,
+        normal_area
+    );
+
+
+    // Replicate / Falsify the results Dae Lee's paper, cite?
+
+};
