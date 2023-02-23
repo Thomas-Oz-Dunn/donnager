c52a4bf3154a1f0b0aa0828eb9aeffc7336ab61f
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-02-23 07:33:41 -0500

    propogate.rs: initial commit

---
+++ src/cosmos/propogate.rs
@@ -0,0 +1,38 @@
+/* 
+Propogator scrap file
+*/
+use crate::cosmos::gravity as grav;
+
+// State
+// Objects- pos, vel, mass, grav field model
+
+pub struct GravConstants {
+    pub geopotential: Geopotential,
+    pub raan_dot: f64,
+    pub argument_of_perigee_dot: f64,
+    pub mean_anomaly_dot: f64,
+    pub c1: f64,
+    pub c4: f64,
+    pub k0: f64,
+    pub k1: f64,
+    pub method: Method,
+    pub orbit_0: grav::Orbit,
+}
+
+
+pub struct Geopotential {
+    pub eq_radius: f64, // Equatorial radius of the earth 
+    pub ke: f64, // square root of earth's grav parameter in earth radii^3 min^-2
+    pub j2: f64, // un-normalised second zonal harmonic
+    pub j3: f64, // un-normalised third zonal harmonic
+    pub j4: f64, // un-normalised fourth zonal harmonic
+}
+
+pub const WGS84: Geopotential = Geopotential {
+    ae: 6378.137,
+    ke: 0.07436685316871385,
+    j2: 0.00108262998905,
+    j3: -0.00000253215306,
+    j4: -0.00000161098761,
+};
+
