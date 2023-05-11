4deac301f45fba62676d981b89fe0550c460d473
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-05-11 19:24:18 -0400

    interplan.py

---
+++ donnager/interplan.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import bokeh as bokeh
+
+def show_trajectory():
+    """
+    Show Interplanetary Misison Trajectory
+    """
+    pass
+
+def show_porkchop_plot():
+    """
+    Show fuel vs time plots
+    """
+    pass
