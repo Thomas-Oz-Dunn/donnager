1e4b5fbd91de13f16fe39e57f12d7acaa25cd422
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-05-25 22:38:10 -0400

    lib.rs: populate interface

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,11 @@ pub mod donnager;
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn calc_earth_day_length(: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn calc_earth_day_length(lattitude_deg: f64, longitude_deg: f64, julian_day: i32) -> PyResult<f64> {
+    let hours = donnager::spacetime::calc_earth_day_length(
+        lattitude_deg, 
+        longitude_deg, 
+        julian_day);
 }
 
 #[pymodule]
