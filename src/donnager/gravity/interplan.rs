3cecfb2bf74c1f1cb9b8e88249f2d5b1884ddfd4
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-06-09 18:13:40 -0400

    interplan.rs: migrate get_ephemeris()

--- src/donnager/gravity/interplan.rs
+++ src/donnager/gravity/interplan.rs
@@ -2,12 +2,11 @@
 Interplanetary Mission Planner
 */
 use std::f64::consts::PI;
-use polars::prelude::*;
+use na::Vector;
 use chrono::{DateTime, Utc};
 use nalgebra::{Vector3, Matrix3, self as na};
 
 use crate::donnager::{
-    constants as cst, 
     gravity::kepler as kepler, 
     spacetime as xyzt};
 
@@ -29,60 +28,6 @@ pub fn calc_esc_vel(
     return (grav_param * (2. / orb_radius_0 - 1. / mean_radius)).sqrt();
 }
 
-/// Get ephemeris dataframe
-pub fn get_ephemeris() -> DataFrame {
-
-    let ephemeris = df! (
-        "number" => &[1, 2, 3, 4, 5, 6, 7, 8],
-        "names" => &["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"],
-        "groups" => &["Inner", "Inner", "Inner", "Inner", "Outer", "Outer", "Outer", "Outer"],
-        "type" => &["Rocky", "Rocky", "Rocky", "Rocky", "Gas Giant", "Gas Giant", "Ice Giant", "Ice Giant"],
-        "mass" => &[
-            cst::MERCURY::MASS, 
-            cst::VENUS::MASS, 
-            cst::EARTH::MASS, 
-            cst::MARS::MASS, 
-            cst::JUPITER::MASS, 
-            cst::SATURN::MASS, 
-            cst::URANUS::MASS, 
-            cst::NEPTUNE::MASS],
-
-        "orbit_semi_major" => &[
-            cst::MercurySunOrbit::SEMI_MAJOR, 
-            cst::VenusSunOrbit::SEMI_MAJOR, 
-            cst::EarthSunOrbit::SEMI_MAJOR, 
-            cst::MarsSunOrbit::SEMI_MAJOR, 
-            None, 
-            None, 
-            None, 
-            None],
-
-        "orbit_ecc" => &[
-            cst::MercurySunOrbit::ECC, 
-            cst::VenusSunOrbit::ECC, 
-            cst::EarthSunOrbit::ECC, 
-            cst::MarsSunOrbit::ECC, 
-            None, 
-            None, 
-            None, 
-            None],
-        
-        "argument_perigelion" => &[
-            cst::MercurySunOrbit::ARG_PERIHELION, 
-            cst::VenusSunOrbit::ARG_PERIHELION, 
-            cst::EarthSunOrbit::ARG_PERIHELION, 
-            cst::MarsSunOrbit::ARG_PERIHELION, 
-            None, 
-            None, 
-            None, 
-            None],
-
-    ).unwrap();
-
-    return ephemeris
-}
-
-
 /// Calculate orbital transfer porkchop plots
 /// 
 /// Inputs
@@ -100,24 +45,24 @@ pub fn calc_porkchop_plots(
     stop_date_time: DateTime<Utc>,
     orbit_1: kepler::Orbit,
     orbit_2: kepler::Orbit
-) -> Vector3<f64> {
+) -> Vec<f64> {
     let frame = xyzt::ReferenceFrames::InertialCartesian;
     
     for launch_time in start_date_time.timestamp()..stop_date_time.timestamp() {
         let radius_1: Vector3<f64> = orbit_1.calc_motion(launch_time as f64, frame)[0];
         let radius_2: Vector3<f64> = orbit_2.calc_motion(launch_time as f64, frame)[0];
-        let v_inf: f64 = calc_esc_vel(
-            cst::SUN::GRAV_PARAM,
-            radius_1.norm(), 
-            radius_2.norm());
+
+        let lambert_vels = lambert_solve(
+            orbit_1, 
+            orbit_2, 
+            1.0);
+                
+        let dv_dpt = lambert_vels[0] - orbit_1.calc_motion(start_date_time as f64, frame)[1];
+        let dv_arr = lambert_vels[1] - orbit_1.calc_motion(stop_date_time as f64, frame)[1];
+        let dv_tot = dv_dpt.norm() + dpt_arr.norm();
         }
-
-    let lambert = lambert_solve(orb_dpt, orb_arr);
-            
-    let dv_dpt = man_lambert.impulses[0][1].norm();
-    let dv_arr = man_lambert.impulses[1][1].norm();
-
-    return [dv_dpt, dv_arrs]
+        
+    return dv_tot
 }
 
 
@@ -134,12 +79,11 @@ pub fn calc_porkchop_plots(
 /// Returns
 /// -------
 /// 
-
 pub fn lambert_solve(
     orbit_i: kepler::Orbit,
     orbit_f: kepler::Orbit,
     prograde_sign: f64
-){
+) -> (Vector3<f64>, Vector3<f64>) {
     let k: f64 = orbit_i.central_body.grav_param;
     let r_i: Vector3<f64> = orbit_i.calc_motion(
         0.0, xyzt::ReferenceFrames::InertialCartesian)[0];
@@ -160,7 +104,7 @@ pub fn lambert_solve(
     let u_h: Vector3<f64> = i_h / i_h.norm();
 
     let min: f64 = (1.0f64).min(c_norm / semi_perim);
-    let mut ll: f64 = prograde_sign * (1.0 - min).powf(0.5);
+    let mut ll: f64 = prograde_sign * (1.0 - min).sqrt();
     if u_h[2] < 0.0 {
         ll: f64 = -ll;
         let u_t_i: Vector3<f64> = prograde_sign * u_r_i.cross(&u_h);
@@ -171,11 +115,10 @@ pub fn lambert_solve(
         let u_t_f: Vector3<f64> = prograde_sign * u_h.cross(&u_r_f);
     }
 
-    let time = tof * ((2. * k / semi_perim.powi(3)).powf(0.5) as i32);
-
+    let time = tof * ((2. * k / semi_perim.powi(3)).sqrt() as i32);
     
     let M_max: f64 = (time / PI).floor();
-    let T_00: f64 = ll.acos() + ll * (1. - ll.powi(2)).powf(0.5);  // T_xM
+    let T_00: f64 = ll.acos() + ll * (1. - ll.powi(2)).sqrt();  // T_xM
     
     // FIXME-TD: Translate into Rust -V
     // # Refine maximum number of revolutions if necessary
@@ -233,11 +176,11 @@ pub fn lambert_solve(
     //         return x
     //     x = p
 
-    let y: f64 = (1. - ll.powi(2) * (1. - x.powi(2))).powf(0.5);
+    let y: f64 = (1. - ll.powi(2) * (1. - x.powi(2))).sqrt();
 
-    let gamma: f64 = (k * semi_perim / 2.).powf(0.5);
+    let gamma: f64 = (k * semi_perim / 2.).sqrt();
     let rho: f64 = (r_i_norm - r_f_norm) / c_norm;
-    let sigma: f64 = (1. - rho.powi(2)).powf(0.5);
+    let sigma: f64 = (1. - rho.powi(2)).sqrt();
 
     let v_r_i = gamma * ((ll * y - x) - rho * (ll * y + x)) / r_i;
     let v_r_f = -gamma * ((ll * y - x) + rho * (ll * y + x)) / r_f;
