df69117b313b45e4f805f38d881750b56333afd5
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-06-09 18:11:55 -0400

    lib.rs: add pyresult wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
 
 pub mod donnager;
 
-
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn calc_earth_day_length(lattitude_deg: f64, longitude_deg: f64, julian_day: i32) -> PyResult<f64> {
+fn calc_earth_day_length(
+    lattitude_deg: f64, 
+    longitude_deg: f64, 
+    julian_day: i32
+) -> PyResult<f64> {
     let hours = donnager::spacetime::calc_earth_day_length(
         lattitude_deg, 
         longitude_deg, 
         julian_day);
+    return PyResult(hours)
 }
 
 #[pymodule]
