e6d8864b1c095907a07b51cf91ea3afa23fdcab4
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-06-17 14:12:19 -0400

    interplan.rs refactor interface for lambert solver to use observation vectors

--- src/donnager/gravity/interplan.rs
+++ src/donnager/gravity/interplan.rs
@@ -2,12 +2,10 @@
 Interplanetary Mission Planner
 */
 use std::f64::consts::PI;
-use na::Vector;
 use chrono::{DateTime, Utc, Timelike};
-use nalgebra::{Vector3, Matrix3, self as na};
+use nalgebra::{Vector3};
 
 use crate::donnager::{
-    constants as cst,
     gravity::kepler as kepler, 
     spacetime as xyzt};
 
@@ -29,26 +27,41 @@ pub fn calc_esc_vel(
     return (grav_param * (2. / orb_radius_0 - 1. / mean_radius)).sqrt();
 }
 
-/// Calculate orbital transfer porkchop plots
+/// Calculate delta v for mission
 /// 
 /// Inputs
 /// ------
-/// datetime_launch_window: `(DateTime<Utc>, DateTime<Utc>)`
-///     Start and end datetime for plot
+/// start_date_time: `DateTime<Utc>`
+///     Start datetime for mission
+/// 
+/// stop_date_time: `DateTime<Utc>`
+///     stop datetime for mission
 /// 
 /// orbit_1: `Orbit`
 ///     Orbit of starting planet
 /// 
 /// orbit_2: `Orbit`
 ///     Orbit of ending planet
-pub fn calc_porkchop_plots(
+pub fn calc_mission_delta_v(
     start_date_time: DateTime<Utc>,
     stop_date_time: DateTime<Utc>,
     orbit_i: kepler::Orbit,
     orbit_f: kepler::Orbit
 ) -> f64 {
     // TODO-TD: parallelize across start stop datetimes using rayon
-    let (dv1, dv2) = lambert_solve(orbit_i, orbit_f, 1.0);
+    let grav_param: f64 = orbit_i.central_body.grav_param;
+    let r_i: Vector3<f64> = orbit_i.calc_motion(
+        0.0, xyzt::ReferenceFrames::InertialCartesian)[0];
+    let r_f: Vector3<f64> = orbit_f.calc_motion(
+        0.0, xyzt::ReferenceFrames::InertialCartesian)[0];
+    let tof = (orbit_f.epoch - orbit_i.epoch).num_seconds() as f64;
+    let (dv1, dv2) = lambert_solve(
+        grav_param,  
+        r_i,  
+        r_f, 
+        tof, 
+        1.0);
+        
     
     let frame = xyzt::ReferenceFrames::InertialCartesian;
     let dv_dpt = dv1 - orbit_i.calc_motion(start_date_time.second() as f64, frame)[1];
@@ -62,29 +75,37 @@ pub fn calc_porkchop_plots(
 ///
 /// Inputs
 /// ------
-/// orbit_i: `kepler::Orbit`
-///     Initial orbit
-///
-/// orbit_f: `kepler::Orbit`
-///     Final orbit
-///
+/// grav_param: `decimal
+///     Central Grav param of system
+/// 
+/// r_i: `[xyz,] decimal`
+///     Initial position vector
+/// 
+/// r_f: `[xyz,] decimal`
+///     Final position vector
+/// 
+/// tof: `decimal`
+///     Time of flight in seconds
+/// 
 /// prograde_sign: `f64`
 ///     Prograde vs retrograde
 /// 
 /// Returns
 /// -------
 /// 
+/// Sources
+/// ------- 
+/// link to polisatro's repo
+/// 
+/// link to izzo's paper
+/// 
 pub fn lambert_solve(
-    orbit_i: kepler::Orbit,
-    orbit_f: kepler::Orbit,
+    grav_param: f64,
+    r_i: Vector3<f64>,
+    r_f: Vector3<f64>,
+    tof: f64,
     prograde_sign: f64
 ) -> (Vector3<f64>, Vector3<f64>) {
-    let grav_param: f64 = orbit_i.central_body.grav_param;
-    let r_i: Vector3<f64> = orbit_i.calc_motion(
-        0.0, xyzt::ReferenceFrames::InertialCartesian)[0];
-    let r_f: Vector3<f64> = orbit_f.calc_motion(
-        0.0, xyzt::ReferenceFrames::InertialCartesian)[0];
-    let tof = orbit_f.epoch - orbit_i.epoch;
     let chord: Vector3<f64> = r_f - r_i;
 
     // Normalize operations
@@ -104,6 +125,7 @@ pub fn lambert_solve(
     let mut u_t_i: Vector3<f64>;
     let mut u_t_f: Vector3<f64>;
 
+    // Orient tangential vectors
     if u_h[2] < 0.0 {
         lambda = -lambda;
         u_t_i = prograde_sign * u_r_i.cross(&u_h);
@@ -115,9 +137,9 @@ pub fn lambert_solve(
     }
 
     // Dimensionless time parameters
-    let time = tof.num_seconds() as f64 * ((2. * grav_param / semi_perim.powi(3)).sqrt());
+    let time = tof * ((2. * grav_param / semi_perim.powi(3)).sqrt());
     
-    let x = find_xy(time, &lambda);
+    let x = find_xy(time, lambda);
     let y: f64 = (1. - lambda.powi(2) * (1. - x.powi(2))).sqrt();
 
     let gamma: f64 = (grav_param * semi_perim / 2.).sqrt();
@@ -223,6 +245,7 @@ fn find_xy(time: f64, lambda: f64) ->  Vector3<f64>{
 mod interplan_tests {
 
     use super::*;
+    use crate::donnager::constants as cst;
 
     #[test]
     fn test_calc_esc_vel()
@@ -232,6 +255,14 @@ mod interplan_tests {
         let orb_radius_f: f64 = 2000000.;
         let esc_vel: f64 = calc_esc_vel(grav_param, orb_radius_0, orb_radius_f);
         assert_eq!(esc_vel, 13302555.410020536);
+    }
+
+    #[test]
+    fn test_calc_mission_delta_v(){
+        // let earth_orbit
+        // let mars_orbit
+        // let start_date
+        // let stop_date
     }
 
 
