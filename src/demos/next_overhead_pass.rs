be78d557a0ca04644ed1b6257dcb9ad7f8e7ef7f
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-08-31 17:42:50 -0400

    next_overhead_pass.rs: implement cli

--- src/demos/next_overhead_pass.rs
+++ src/demos/next_overhead_pass.rs
@@ -1,6 +1,7 @@
 /*
 Calculate next overhead pass windows
 */
+
 use clap::Parser;
 
 use nalgebra::Vector3;
@@ -10,8 +11,19 @@ use donnager::donnager::{
     constants as cst, 
     spacetime as xyzt};
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Ground station
+    #[arg(short, long)]
+    ground_station: Vector3<f64>,
+
+}
 
 fn main() {
+    
+    let args = Args::parse();
+
     // Orbiting Body
     let tle_str: &str = "ISS
     1 25544U 98067A   23060.72453421  .00027779  00000-0  50068-3 0  9993
@@ -28,10 +40,10 @@ fn main() {
     // Stop time
 
     // Ground station
-
+    
     let p_ecef = orbit.calc_motion(time_since_epoch, frame, 0);
 
-    let ground_station: Vector3<f64> = vec![lat,   lon ,  height];
+    let ground_station: Vector3<f64> = args.ground_station;
 
     
 
