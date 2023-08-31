f0751a57b313604fb1deaf1ca4188dcee5e64d7b
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-08-30 21:10:02 -0400

    next_overhead_pass.rs:  add cli parser library

--- src/demos/next_overhead_pass.rs
+++ src/demos/next_overhead_pass.rs
@@ -1,12 +1,12 @@
 /*
 Calculate next overhead pass windows
 */
-
+use clap::Parser;
 
 use nalgebra::Vector3;
 
 use donnager::donnager::{
-    gravity as grav,
+    gravity::kepler as kepler,
     constants as cst, 
     spacetime as xyzt};
 
@@ -17,8 +17,17 @@ fn main() {
     1 25544U 98067A   23060.72453421  .00027779  00000-0  50068-3 0  9993
     2 25544  51.6419 140.8390 0005926  43.3718 100.9839 15.49547192385127";
     
-    let orbit: grav::kepler::Orbit = grav::kepler::Orbit::from_tle(tle_str.to_string());
+    let orbit: kepler::Orbit = kepler::Orbit::from_tle(
+        tle_str.to_string()
+    );
+
     let frame = xyzt::ReferenceFrames::Cartesian;
+    
+    // Start time
+
+    // Stop time
+
+    // Ground station
 
     let p_ecef = orbit.calc_motion(time_since_epoch, frame, 0);
 
