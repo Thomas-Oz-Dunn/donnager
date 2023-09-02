557056c1f711ed4bfc3c6142b5bf6ba5140cdc7e
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-09-02 13:31:51 -0400

    all: debug build and cli

--- src/demos/next_overhead_pass.rs
+++ src/demos/next_overhead_pass.rs
@@ -14,9 +14,22 @@ use donnager::donnager::{
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// Ground station
+    /// tle string
+    #[arg(short, long)]
+    tle_string: String,
+
+    /// Ground station lat lon height
     #[arg(short, long)]
     ground_station: Vector3<f64>,
+
+    /// start date of window
+    #[arg(short, long)]
+    start_date: Vector3<u32>,
+
+    /// stop date of window
+    #[arg(short, long)]
+    stop_date: Vector3<u32>,
+
 
 }
 
@@ -25,9 +38,15 @@ fn main() {
     let args = Args::parse();
 
     // Orbiting Body
-    let tle_str: &str = "ISS
-    1 25544U 98067A   23060.72453421  .00027779  00000-0  50068-3 0  9993
-    2 25544  51.6419 140.8390 0005926  43.3718 100.9839 15.49547192385127";
+    let tle_str: &str;
+    if args.tle_string == None {
+
+        tle_str = "ISS
+        1 25544U 98067A   23060.72453421  .00027779  00000-0  50068-3 0  9993
+        2 25544  51.6419 140.8390 0005926  43.3718 100.9839 15.49547192385127";
+    } else {
+        tle_str = args.tle_string
+    }
     
     let orbit: kepler::Orbit = kepler::Orbit::from_tle(
         tle_str.to_string()
@@ -39,10 +58,10 @@ fn main() {
 
     // Stop time
 
-    // Ground station
     
-    let p_ecef = orbit.calc_motion(time_since_epoch, frame, 0);
-
+    let p_ecef: Vector3<f64> = orbit.calc_motion(time_since_epoch, frame, 0);
+    
+    // Ground station
     let ground_station: Vector3<f64> = args.ground_station;
 
     
