10a8d3c47aa34d9a8e23da2b4d309e46d1fc066a
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2023-09-16 19:30:08 -0400

    math.rs: lint hyp2f1b

--- src/donnager/math.rs
+++ src/donnager/math.rs
@@ -3,21 +3,20 @@ Mathematical support
 */
 
 
+
 /// Hypergeometric function 2F1(3, 1, 5/2, x), see [Battin].
 /// https://en.wikipedia.org/wiki/Hypergeometric_function
 pub fn hyp2f1b(x: f64) -> f64 {
 
     if x >= 1.0{
         return f64::INFINITY;
-    }else{
+    } else {
         let mut res: f64 = 1.0;
-        let mut res_old: f64;
         let mut term: f64 = 1.0;
         let mut ii: f64 = 0.;
 
         while term > 0.{
-            term = term * (3. + ii) * (1. + ii) / (5. / 2. + ii) * x / (ii + 1.);
-            res_old = res;
+            term *= (3. + ii) * (1. + ii) / (5. / 2. + ii) * x / (ii + 1.);
             res += term;
             ii += 1.;
         }
