38e20fc8ebf97f6e75541976ec9178fd36d5533b
Thomas.Oz.Dunn <thomasjohndunn@yahoo.com>
2023-12-12 20:01:41 -0500

    overhead_pass.rs: interface tle code sgp4 and eclipse code

--- src/apps/overhead_pass.rs
+++ src/apps/overhead_pass.rs
@@ -1,3 +1,4 @@
+use nalgebra::Vector;
 use nalgebra::Vector3;
 use sgp4;
 use parse_tle::tle;
@@ -7,7 +8,11 @@ use donnager::donnager::constants as cst;
 
 fn main() {
 
-    let observer_lla: Vector3<f64> = Vector3::new(28.396837, -80.605659, 0.0);
+    let observer_lla: Vector3<f64> = Vector3::new(
+        28.396837, 
+        -80.605659, 
+        0.0
+    );
 
     // Earth
     let earth: xyzt::Body = xyzt::Body {
@@ -24,27 +29,47 @@ fn main() {
     // - Datetime
     // - Lat lon
     // - TLE of interest
-
-    let tle_str = ;
+    let date_time: Datetime<Utc>;
+    let tle_str: &str = "";
     let tle: tle::TLE = tle::parse(tle_str);
+    let arg_peri: f64 = tle.arg_perigee;
 
-    let elements: sgp4::Elements = 
+    let elements: sgp4::Elements = sgp4::Elements{
+        object_name: Some(tle.name),
+        international_designator: Option<String>,
+        norad_id: u64,
+        classification: Classification,
+        datetime: NaiveDateTime,
+        mean_motion_dot: tle.mean_motion_1,
+        mean_motion_ddot: tle.mean_motion_2,
+        drag_term: tle.radiation_pressure,
+        element_set_number: u64,
+        inclination: tle.inc,
+        right_ascension: tle.raan,
+        eccentricity: tle.eccentricity,
+        argument_of_perigee: tle.arg_perigee,
+        mean_anomaly: tle.mean_anomaly,
+        mean_motion: tle.mean_motion,
+        revolution_number: u64,
+        ephemeris_type: u8,
+    }; 
 
-    let constants: sgp4::Constants = sgp4::Constants::from_elements(&elements).unwrap();
+    let constants: sgp4::Constants = sgp4::Constants::from_elements(
+        &elements
+    ).unwrap();
 
-    for hours in 0..24 {
-        println!("t = {} min", hours * 60);
-        let prediction = constants.propagate(sgp4::MinutesSinceEpoch((hours * 60) as f64)).unwrap();
-        println!("    r = {:?} km", prediction.position);
-        println!("    ṙ = {:?} km.s⁻¹", prediction.velocity);
+    let p_eci: Vector3<f64> = Vector3::new(0., 0., 0.);
+    for min in 0..24*60 {
+        println!("t = {} min", min);
+        let minutes_since_epoch: sgp4::MinutesSinceEpoch = sgp4::MinutesSinceEpoch(min as f64);
+        let prediction = constants.propagate(minutes_since_epoch).unwrap();
+        let p_eci: [f64; 3] = prediction.position;
     }
 
-
-
-
-    let j2000_days: f64 = julian_day - cst::J2000_DAY;
-
-
+    let is_bright: bool = xyzt::is_eclipsed_by_earth(
+        p_eci, 
+        date_time
+    );
     let observer_ecef: Vector3<f64> = xyzt::planetodetic_to_cartesian_rotational(
         observer_lla, 
         cst::EARTH::RADIUS_EQUATOR, 
@@ -53,8 +78,9 @@ fn main() {
 
     let eci_to_ecef = xyzt::calc_inertial_rotational_rotam(
         date_time, 
-        rot_rate_rad_day
+        cst::EARTH::ROT_RATE * 60. * 60. * 24. 
     );
+
     let p_ecef: Vector3<f64> = eci_to_ecef * p_eci;
     let enu: Vector3<f64> = xyzt::fixed_frame_to_enu(
         observer_lla, 
