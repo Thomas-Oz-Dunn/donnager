7ffbce81beefd2f20fa4c10ce0de59d41e8029ad
Thomas.Oz.Dunn <thomasjohndunn@yahoo.com>
2023-12-14 18:52:38 -0500

    overhead_pass.rs: add classification level interface

--- src/apps/overhead_pass.rs
+++ src/apps/overhead_pass.rs
@@ -1,4 +1,3 @@
-use nalgebra::Vector;
 use nalgebra::Vector3;
 use sgp4;
 use parse_tle::tle;
@@ -25,7 +24,7 @@ fn main() {
     };
 
 
-    // CLI inputs
+    // Parse CLI inputs
     // - Datetime
     // - Lat lon
     // - TLE of interest
@@ -34,24 +33,35 @@ fn main() {
     let tle: tle::TLE = tle::parse(tle_str);
     let arg_peri: f64 = tle.arg_perigee;
 
+    let class_level: sgp4::Classification = match tle.classification.as_str() {
+        "U" => sgp4::Classification::Unclassified,
+        "C" => sgp4::Classification::Classified,
+        "S" => sgp4::Classification::Secret,
+        _ => sgp4::Classification::Unclassified
+    };
+    
+
+    // NaiveDateTime = epoch_to_datetime(Epoch)
+
+
     let elements: sgp4::Elements = sgp4::Elements{
         object_name: Some(tle.name),
-        international_designator: Option<String>,
-        norad_id: u64,
-        classification: Classification,
-        datetime: NaiveDateTime,
+        international_designator: Some(tle.international_designator),
+        norad_id: tle.catalog_number.parse::<u64>().expect("None numerical value found"),
+        classification: class_level,
+        datetime: tle.epoch,
         mean_motion_dot: tle.mean_motion_1,
         mean_motion_ddot: tle.mean_motion_2,
         drag_term: tle.radiation_pressure,
-        element_set_number: u64,
+        element_set_number: tle.element_set_number,
         inclination: tle.inc,
         right_ascension: tle.raan,
         eccentricity: tle.eccentricity,
         argument_of_perigee: tle.arg_perigee,
         mean_anomaly: tle.mean_anomaly,
         mean_motion: tle.mean_motion,
-        revolution_number: u64,
-        ephemeris_type: u8,
+        revolution_number: tle.rev_num as u64,
+        ephemeris_type: tle.ephemeris_type,
     }; 
 
     let constants: sgp4::Constants = sgp4::Constants::from_elements(
